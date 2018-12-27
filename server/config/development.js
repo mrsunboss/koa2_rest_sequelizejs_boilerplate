d3e35a44c06ae2729b36c6d6f538ced2c4bbcd6c
Rick Chiang <mr.sunboss@gmail.com>
2018-12-27 13:45:57 +0800

    update

--- server/config/development.js
+++ server/config/development.js
@@ -1,11 +1,16 @@
 export default {
   'port': 3000,
-  'connection': {
-    'dialect': 'sqlite',
-    'storage': './db.test.sqlite',
-    'username': null,
-    'password': null,
-    'database': null,
-    'force': true
-  }
+   'connection': {
+     'username': 'test1',
+     'password': '12345',
+     'database': 'teset1',
+     'host': '127.0.0.1',
+     'port': 3306,
+     'dialect': 'mysql',
+     'dialectOptions': {
+       charset: 'utf8mb4'
+     },
+     'force': true,
+     timestamps: true
+   },
 };
